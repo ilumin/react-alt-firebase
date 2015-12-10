d39852155647e9b877d552983bec5443af7f8f16
Teerasak Vichadee <iolumin@gmail.com>
2015-12-11 00:06:19 +0700

    Set material-ui theme tutorial use old version of mui which is ThemeManager is not a function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 import MessageList from './MessageList.jsx';
 import mui from 'material-ui';
 
-var AppBar = mui.AppBar;
+const {
+  Styles
+} = mui;
+
+const ThemeManager = Styles.ThemeManager;
+const DarkRawTheme = Styles.DarkRawTheme;
+const AppBar = mui.AppBar;
 
 class App extends React.Component {
   constructor () {
     super();
+  }
+
+  static childContextTypes = {
+    muiTheme: React.PropTypes.object
+  };
+
+  getChildContext () {
+    return {
+      muiTheme: ThemeManager.getMuiTheme(DarkRawTheme)
+    };
   }
 
   render () {
