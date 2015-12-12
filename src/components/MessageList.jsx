d7eb642de7d177415c48c9051f60553d7c4c6047
Teerasak Vichadee <iolumin@gmail.com>
2015-12-12 10:20:43 +0700

    Change to realtime firebase

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -17,7 +17,7 @@ class MessageList extends React.Component {
     };
 
     this.firebaseRef = new Firebase('https://luminous-torch-3780.firebaseio.com/messages');
-    this.firebaseRef.once("value", (dataSnapshot) => {
+    this.firebaseRef.on("value", (dataSnapshot) => {
       var messagesVal = dataSnapshot.val();
       var messages = _(messagesVal)
         .keys()
