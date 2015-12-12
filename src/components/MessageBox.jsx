a9d3ef46a18e55ad4f0e2ff4a4313d7ded5751a8
Teerasak Vichadee <iolumin@gmail.com>
2015-12-12 10:18:08 +0700

    Add onChange and onKeyUp event to save message

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import mui from 'material-ui';
+import trim from 'trim';
+import Firebase from 'firebase';
 
 const {
   Card
@@ -8,6 +10,33 @@ const {
 class MessageBox extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      message: ''
+    }
+
+    this.firebaseRef = new Firebase('https://luminous-torch-3780.firebaseio.com/messages');
+  }
+
+  onChange (e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+  onKeyUp (e) {
+    if (e.keyCode === 13 && trim(e.target.value) != '') {
+      e.preventDefault();
+
+      this.firebaseRef.push({
+        message: this.state.message
+      });
+
+      this.setState({
+        message: ''
+      });
+
+      console.log('set messages:', trim(e.target.value));
+    }
   }
 
   render () {
@@ -15,7 +44,11 @@ class MessageBox extends React.Component {
       <Card style={{
           flexGrow: 1
         }}>
-        <textarea style={{
+        <textarea
+          value={this.state.message}
+          onChange={this.onChange.bind(this)}
+          onKeyUp={this.onKeyUp.bind(this)}
+          style={{
             width: '100%',
             display: 'block',
             boxSizing: 'border-box',
@@ -24,7 +57,7 @@ class MessageBox extends React.Component {
             resize: 'none',
             minHeight: 50,
             outline: 'auto 0px'
-          }}/>
+          }} />
       </Card>
     );
   }
