f41feabaa6c5a08c0a6f74f949c437c02f3310b7
Teerasak Vichadee <iolumin@gmail.com>
2015-12-13 11:04:08 +0700

    Add sources logic to send message to firebase

--- src/sources/MessageSource.js
+++ src/sources/MessageSource.js
@@ -5,7 +5,21 @@ let firebaseRef = null;
 let MessageSource = {
   sendMessage: {
     remote(state) {
-      
+      return new Promise((resolve, reject) => {
+        if (!firebaseRef) {
+          resolve();
+        }
+
+        firebaseRef.push({
+          "message": state.message,
+          "date": new Date().toUTCString(),
+          "author": state.user.google.displayName,
+          "userId": state.user.uid,
+          "profilePic": state.user.google.profileImageURL
+        });
+
+        resolve();
+      });
     },
     success: Actions.messageSendSuccess,
     error: Actions.messageSendError
