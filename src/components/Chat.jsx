6cb7c4a837dea5edabba96c3ef149aea680095f0
Teerasak Vichadee <iolumin@gmail.com>
2015-12-13 11:57:17 +0700

    Update Chat component to check user state if not exist redirect to login

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import MessageList from './MessageList.jsx';
 import ChannelList from './ChannelList.jsx';
 import MessageBox from './MessageBox.jsx';
+import ChatStore from '../stores/ChatStore';
 
 class Chat extends React.Component {
+  static willTransitionTo(transition) {
+    let state = ChatStore.getState();
+    if (!state.user) {
+      transition.redirect('/login');
+    }
+  }
+
   render() {
     return (
       <div>
